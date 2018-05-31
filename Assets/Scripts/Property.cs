4575ecb4261259411fce431ac2ad63cb1714ec79
AlonsoWinPc <alonsoaman@hotmail.com>
2018-05-31 03:52:37 -0300

    Básico de propriedades foi implementado.

---
+++ Assets/Scripts/Property.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property : MonoBehaviour {
+
+
+
+    public Sprite Castelo, Mina, Vila, Fazenda, Floresta; 
+
+
+
+    
+}
