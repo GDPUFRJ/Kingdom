42bf9d4af48ca87d646ac105d78c85d29ef89b00
TanookiVerde <pedrovitor.marques@gmail.com>
2018-07-15 20:31:07 -0300

    Listagem de Propriedades

--- Assets/Scripts/System/Panels/Telas/PropertyPanel.cs
+++ Assets/Scripts/System/Panels/Telas/PropertyPanel.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 public class PropertyPanel : AMainPanel
 {
     [SerializeField] private GameObject propertyBoxPrefab;
+    [SerializeField] private Transform root;
 
     public override void PrepareContent()
     {
-        print("PREPARING");
+        DeleteAllChilds();
+        List<Property> propertyList = FindObjectOfType<PropertyManager>().Propriedades;
+        foreach(Property prop in propertyList)
+        {
+            if (prop.dominated)
+            {
+                var obj = Instantiate(propertyBoxPrefab, root).GetComponent<PropertyBox>();
+                obj.SetInformation(prop);
+            }
+        }
+    }
+    private void DeleteAllChilds()
+    {
+        for(int i = root.childCount; i > 0; i--)
+        {
+            Destroy(root.GetChild(0).gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/System/PropertyBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PropertyBox : MonoBehaviour {
+    [SerializeField] private Image icon;
+    [SerializeField] private Text title;
+
+    [SerializeField] private Text riq;
+    [SerializeField] private Text con;
+    [SerializeField] private Text food;
+
+    [SerializeField] private Text happiness;
+    [SerializeField] private Text soldier;
+
+    private Property property;
+
+    public void SetInformation(Property prop)
+    {
+        //funcao dentro de toda propriedade que retorna os recursos que ela atualmente gasta/produz
+        //funcao que retorna facilmente seu sprite
+        this.property = prop;
+
+        this.title.text = prop.customTitle;
+    }
+    public Property GetProperty()
+    {
+        return property;
+    }
+}
