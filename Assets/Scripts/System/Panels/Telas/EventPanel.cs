710ae52c4ebfb4e3df7a7b238f612ba199b92a6f
TanookiVerde <pedrovitor.marques@gmail.com>
2018-07-15 19:55:15 -0300

    Refiz o fluxo básico do Menu baseado na reformulação visual do último commit

---
+++ Assets/Scripts/System/Panels/Telas/EventPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPanel : AMainPanel
+{
+    [SerializeField] private GameObject eventBoxPrefab;
+
+    public override void PrepareContent()
+    {
+        print("PREPARING");
+    }
+}
---
+++ Assets/Scripts/System/Panels/Telas/PropertyPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyPanel : AMainPanel
+{
+    [SerializeField] private GameObject propertyBoxPrefab;
+
+    public override void PrepareContent()
+    {
+        print("PREPARING");
+    }
+}
