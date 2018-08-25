c55b0c995299c7688da0d8eb23400fe9c68de967
TanookiVerde <pedrovitor.marques@gmail.com>
2018-08-25 13:02:58 -0300

    Ajustes

--- Assets/Scripts/System/Panels/Telas/SoldierPanel.cs
+++ Assets/Scripts/System/Panels/Telas/SoldierPanel.cs
@@ -1,27 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class SoldierPanel : AMainPanel
 {
     public GameObject CanvasBattle;
+    [SerializeField] private CanvasGroup editButtonsGroup;
+    [SerializeField] private Image toggle;
+    [Space(5)]
+    [SerializeField] private float editButtonsFadeTime = 0.3f;
+    [SerializeField] private Color editEnabledColor = Color.green;
+    [SerializeField] private Color editDisabledColor = Color.red;
+
+    private bool isEditButtonsEnable = false;
 
     private void Start()
     {
+        InitializeEditToggle();
         CanvasBattle.SetActive(false);
     }
-
     public override void PrepareContent()
     {
-        print("PREPARING");
-        TimerPanel.SetPause(true);
         CanvasBattle.SetActive(true);
-        
     }
-
     public override void UnprepareContent()
     {
         CanvasBattle.SetActive(false);
-        TimerPanel.SetPause(false);
+    }
+    private void InitializeEditToggle()
+    {
+        isEditButtonsEnable = false;
+        toggle.color = isEditButtonsEnable ? editEnabledColor : editDisabledColor;
+        editButtonsGroup.interactable = isEditButtonsEnable;
+        editButtonsGroup.blocksRaycasts = isEditButtonsEnable;
+        editButtonsGroup.DOFade(0, 0);
+    }
+    public void ToggleEditButtons()
+    {
+        print("AEW");
+        isEditButtonsEnable = !isEditButtonsEnable;
+        TimerPanel.SetPause(isEditButtonsEnable);
+        toggle.color = isEditButtonsEnable ? editEnabledColor : editDisabledColor;
+        float target = isEditButtonsEnable ? 1 : 0;
+        editButtonsGroup.interactable = isEditButtonsEnable;
+        editButtonsGroup.blocksRaycasts = isEditButtonsEnable;
+        editButtonsGroup.DOFade(target, editButtonsFadeTime);
     }
 }
