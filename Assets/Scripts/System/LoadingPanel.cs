0fa5eaf872611b46fa04c198afff234fb863cab8
TanookiVerde <pedrovitor.marques@gmail.com>
2018-09-23 19:43:20 -0300

    Menu Principal para ingles ver

---
+++ Assets/Scripts/System/LoadingPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class LoadingPanel : MonoBehaviour {
+    private const float MIN_LOADING_TIME = 1f;
+    [SerializeField] private CanvasGroup group;
+
+    public void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        group.blocksRaycasts = false;
+        group.DOFade(0, 0);
+    }
+    public void Open(string sceneName)
+    {
+        group.blocksRaycasts = true;
+        group.DOFade(1, 0.5f);
+        StartCoroutine(LoadNewScene(sceneName));
+    }
+    public void Hide()
+    {
+        group.blocksRaycasts = false;
+        group.DOFade(0, 0.5f);
+        Destroy(this.gameObject, 0.5f);
+    }
+    private IEnumerator LoadNewScene(string name)
+    {
+        yield return new WaitForSeconds(MIN_LOADING_TIME);
+        SceneManager.LoadSceneAsync(name);
+    }
+}
---
+++ Assets/Scripts/System/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenu : MonoBehaviour {
+    private const float FADE_DURATION = 0.5f;
+    private const float MAP_SPEED = 5f;
+    [SerializeField] private List<CanvasGroup> groups;
+    [SerializeField] private CanvasGroup transitionPanel;
+    [SerializeField] private LoadingPanel loadingPanel;
+
+    private void Start()
+    {
+        transitionPanel.DOFade(1, 0);
+        transitionPanel.DOFade(0, FADE_DURATION * 3);
+        ToggleGroup(groups[0], true, 0);
+        ToggleGroup(groups[1], false, 0);
+    }
+    public void TouchToStart()
+    {
+        ToggleGroup(groups[0], false);
+        ToggleGroup(groups[1], true);
+    }
+    public void Continue()
+    {
+        loadingPanel.Open("SampleScene");
+    }
+    public void NewGame()
+    {
+        print("New Game");
+    }
+    public void Credits()
+    {
+        print("Credits");
+    }
+    public void Options()
+    {
+        print("Options");
+    }
+    private void ToggleGroup(CanvasGroup group, bool b, float duration = FADE_DURATION)
+    {
+        float i = b ? 1 : 0;
+        group.DOFade(i, duration);
+        group.interactable = b;
+        group.blocksRaycasts = b;
+    }
+}
