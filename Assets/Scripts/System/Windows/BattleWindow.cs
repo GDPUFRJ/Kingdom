fda43451128e540026e6d01be9b487613d794975
Vítor Barcellos <vtor.barcellos7@gmail.com>
2018-12-04 09:00:06 -0200

    Atualizando a tela de batalha com as artes novas e fazendo a animação final da tela de batalha

--- Assets/Scripts/System/Windows/BattleWindow.cs
+++ Assets/Scripts/System/Windows/BattleWindow.cs
@@ -6,19 +6,23 @@ using DG.Tweening;
 
 public class BattleWindow : MonoBehaviour {
 	[Header("Animation Preferences")]
-	[SerializeField] private float timeToShowAndHideShield;
-	[SerializeField] private float shieldDelay;
-	[SerializeField] private float timeToGrowBattleScene;
+	//[SerializeField] private float timeToShowAndHideShield;
+	//[SerializeField] private float shieldDelay;
+	//[SerializeField] private float timeToGrowBattleScene;
 	[SerializeField] private float backgroundInitialPosition;
 	[SerializeField] private float backgroundFinalPosition;
 	[SerializeField] private float timeToShowPlayerBattlePoints;
 	[SerializeField] private float timeToShowEnemyBattlePoints;
 
 	[Header("UI Objects")]
-	[SerializeField] private CanvasGroup shield;
+    [SerializeField] private Image sword1;
+    [SerializeField] private Image sword2;
+    [SerializeField] private Animator swordsCrossingAnimator;
+    //[SerializeField] private CanvasGroup shield;
 	[SerializeField] private CanvasGroup background;
-	[SerializeField] private RectTransform maskBackground;
-	[SerializeField] private RectTransform battleScene;
+	//[SerializeField] private RectTransform maskBackground;
+	//[SerializeField] private RectTransform battleScene;
+    [SerializeField] private List<Image> scoreFlags;
 	[SerializeField] private Text playerBattlePointsText;
 	[SerializeField] private Text enemyBattlePointsText;
 	[SerializeField] private Transform player;
@@ -48,46 +52,66 @@ public class BattleWindow : MonoBehaviour {
 	private IEnumerator ShowBattleScene()
 	{
 		InitializeAllValues();
-		yield return ShowAndHideShield();
-		yield return AnimateBattleSceneAndBackground();
-		yield return ShowBattlePoints();
-		yield return Battle();
-		yield return ShowResults();
+        //yield return ShowAndHideShield();
+        //yield return AnimateBattleSceneAndBackground();
+        //yield return ShowBattlePoints();
+        //yield return Battle();
+        //yield return ShowResults();
+        yield return ShowSwordsCrossing();
+        yield return ShowBattlePoints();
+        yield return Battle();
+        yield return ShowUpdatedBattlePoints();
+        yield return ShowResult();
     }
-	private void InitializeAllValues()
+    private void InitializeAllValues()
 	{
-		battleScene.DOSizeDelta(new Vector2(720,0),0);
-		shield.transform.DOScale(0,0);
-		results.DOFade(0,0);
-		results.blocksRaycasts = false;
+		//battleScene.DOSizeDelta(new Vector2(720,0),0);
+		//shield.transform.DOScale(0,0);
+		//results.DOFade(0,0);
+		//results.blocksRaycasts = false;
 		background.DOFade(0,0);
 		background.blocksRaycasts = false;
-		maskBackground.DOAnchorPosX(backgroundInitialPosition,0.5f);
+        sword1.DOColor(Color.white, 0);
+        sword2.DOColor(Color.white, 0);
+        sword1.DOFade(0, 0);
+        sword2.DOFade(0, 0);
+        //maskBackground.DOAnchorPosX(backgroundInitialPosition,0.5f);
 		playerBattlePointsText.transform.DOScale(0,0);
 		enemyBattlePointsText.transform.DOScale(0,0);
 		playerBattlePointsText.text = playerBattlePoints.ToString();
 		enemyBattlePointsText.text = enemyBattlePoints.ToString();
-        enemy.DOScale(1f, 0.3f);
-        player.DOScale(1f, 0.3f);
+        foreach(Image im in scoreFlags)
+        {
+            im.DOFade(0, 0);
+        }
+        //enemy.DOScale(1f, 0.3f);
+        //player.DOScale(1f, 0.3f);
     }
-	private IEnumerator ShowAndHideShield()
-	{
-		background.DOFade(1,0.5f);
-		background.blocksRaycasts = true;
-		shield.transform.DOScale(1,timeToShowAndHideShield/2);
-		yield return new WaitForSeconds(timeToShowAndHideShield/2 + shieldDelay);
-		shield.transform.DOScale(0,timeToShowAndHideShield/2);
-	}
-	private IEnumerator AnimateBattleSceneAndBackground()
-	{
-		battleScene.DOSizeDelta(new Vector2(720,680),timeToGrowBattleScene);
-		maskBackground.DOAnchorPosX(backgroundFinalPosition,1f);
-		yield return new WaitForSeconds(timeToShowAndHideShield/2);
-		yield return new WaitForSeconds(Mathf.Abs( (timeToShowAndHideShield/2) - timeToGrowBattleScene ));
-	}
+	//private IEnumerator ShowAndHideShield()
+	//{
+	//	background.DOFade(1,0.5f);
+	//	background.blocksRaycasts = true;
+	//	shield.transform.DOScale(1,timeToShowAndHideShield/2);
+	//	yield return new WaitForSeconds(timeToShowAndHideShield/2 + shieldDelay);
+	//	shield.transform.DOScale(0,timeToShowAndHideShield/2);
+	//}
+	//private IEnumerator AnimateBattleSceneAndBackground()
+	//{
+	//	battleScene.DOSizeDelta(new Vector2(720,680),timeToGrowBattleScene);
+	//	maskBackground.DOAnchorPosX(backgroundFinalPosition,1f);
+	//	yield return new WaitForSeconds(timeToShowAndHideShield/2);
+	//	yield return new WaitForSeconds(Mathf.Abs( (timeToShowAndHideShield/2) - timeToGrowBattleScene ));
+	//}
 	private IEnumerator ShowBattlePoints()
 	{
-		playerBattlePointsText.transform.DOScale(1,timeToShowPlayerBattlePoints);
+        foreach(Image im in scoreFlags)
+        {
+            im.DOFade(1, 0.3f);
+        }
+        enemy.DOScale(1f, 0.3f);
+        player.DOScale(1f, 0.3f);
+        yield return new WaitForSeconds(0.5f);
+        playerBattlePointsText.transform.DOScale(1,timeToShowPlayerBattlePoints);
 		yield return new WaitForSeconds(0.5f);
 		enemyBattlePointsText.transform.DOScale(1,timeToShowPlayerBattlePoints);
 	}
@@ -101,8 +125,8 @@ public class BattleWindow : MonoBehaviour {
 			enemy.DOScale(1.1f,0.3f);
 			player.DOScale(0.9f,0.3f);
 		}
-	}
-	private IEnumerator ShowResults()
+    }
+    private IEnumerator ShowResults()
 	{
 		results.transform.GetChild(0).GetComponent<Text>().text = PlayerIsTheWinner() ? "VENCEDOR" : "PERDEDOR";
 		results.DOFade(1,0.5f);
@@ -112,7 +136,57 @@ public class BattleWindow : MonoBehaviour {
 		GetComponent<CanvasGroup>().blocksRaycasts = false;
         // Destroy(this.gameObject,0.5f);
 	}
-	private bool PlayerIsTheWinner(){
+    private IEnumerator ShowSwordsCrossing()
+    {
+        sword1.DOFade(1, 0.5f);
+        sword2.DOFade(1, 0.5f);
+        swordsCrossingAnimator.Play("SwordsCrossing_show");
+        yield return new WaitForSeconds(0.5f);
+    }
+    private IEnumerator ShowOriginalBattlePoints()
+    {
+        playerBattlePointsText.transform.DOScale(1, timeToShowPlayerBattlePoints);
+        yield return new WaitForSeconds(0.5f);
+        enemyBattlePointsText.transform.DOScale(1, timeToShowPlayerBattlePoints);
+    }
+    private IEnumerator ShowUpdatedBattlePoints()
+    {
+        if (playerBattlePoints > enemyBattlePoints)
+        {
+            playerBattlePoints = playerBattlePoints - enemyBattlePoints;
+            enemyBattlePoints = 0;
+        }
+        else
+        {
+            enemyBattlePoints = enemyBattlePoints - playerBattlePoints;
+            playerBattlePoints = 0;
+        }
+        playerBattlePointsText.text = playerBattlePoints.ToString();
+        enemyBattlePointsText.text = enemyBattlePoints.ToString();
+        yield return new WaitForSeconds(0.5f);
+    }
+    private IEnumerator ShowResult()
+    {
+        //Color color;
+        //if (PlayerIsTheWinner())
+        //{
+        //    color = Color.green;
+        //}
+        //else
+        //{
+        //    color = Color.red;
+        //}
+        //sword1.DOColor(color, 0.5f);
+        //sword2.DOColor(color, 0.5f);
+        if (PlayerIsTheWinner()) swordsCrossingAnimator.Play("SwordsCrossing_win");
+        else swordsCrossingAnimator.Play("SwordsCrossing_lose");
+        yield return new WaitForSeconds(1f);
+        GetComponent<CanvasGroup>().DOFade(0, 0.5f);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        yield return new WaitForSeconds(0.5f);
+        swordsCrossingAnimator.Play("Default");
+    }
+    private bool PlayerIsTheWinner(){
 		if(playerBattlePoints >= enemyBattlePoints){
 			return true;
 		}else{
