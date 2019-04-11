8c359520e6a828580e47006a0bc58f2a02db8625
Vítor Barcellos <vtor.barcellos7@gmail.com>
2019-04-11 09:58:32 -0300

    Começando a colocar os nomes dos locais no mapa

---
+++ Assets/Scripts/System/TranslatedSpriteBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslatedSpriteBehaviour : MonoBehaviour
+{
+    [SerializeField] private Sprite portugueseSprite;
+    [SerializeField] private Sprite englishSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        ChangeText(TranslationManager.GameLanguage);
+    }
+
+    private void ChangeText(Language language)
+    {
+        switch (language)
+        {
+            case Language.Portuguese:
+                spriteRenderer.sprite = portugueseSprite;
+                break;
+            case Language.English:
+                spriteRenderer.sprite = englishSprite;
+                break;
+        }
+    }
+}
