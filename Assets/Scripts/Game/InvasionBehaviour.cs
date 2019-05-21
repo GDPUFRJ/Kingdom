e8f5fbd1313ac148bcbc7e7115facfba09e1021d
Vítor Barcellos <vtor.barcellos7@gmail.com>
2019-05-21 17:59:39 -0300

    Separando estratégias diferentes de invasões de acordo com o tipo de propriedade, e implementando comportamento mais ousado para invasões de quartéis

--- Assets/Scripts/Game/InvasionBehaviour.cs
+++ Assets/Scripts/Game/InvasionBehaviour.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class InvasionBehaviour : MonoBehaviour
 {
-    private Property property;
+    private Property _thisProperty;
 
     private void Start()
     {
-        property = GetComponent<Property>();
+        _thisProperty = GetComponent<Property>();
         TimerPanel.OnDayEnd += OnDayEnd;
     }
 
@@ -16,23 +16,43 @@ public class InvasionBehaviour : MonoBehaviour
     {
         AttemptInvasion();
     }
-
+    
+/// <summary>
+/// Different kinds of properties have different invasion behaviours.
+/// Barracks invade random neighbour enemy properties whatever the odds.
+/// Other kinds of properties only invade if they have more soldiers than their enemy.
+/// </summary>
     private void AttemptInvasion()
     {
-        if (!property.dominated &&
-            HasDominatedNeighbor() &&
-            property.GetSoldiers(SoldierType.Enemy) == 0 &&
-            Random.Range(0f, 1f) < PropertyManager.Instance.invasionChancePerProperty &&
-            property.GetSoldiers(SoldierType.InProperty) > 0 &&
-            property.GetSoldiers(SoldierType.InProperty) > (GetRandomDominatedNeighbor().GetSoldiers(SoldierType.InProperty) / 2))
+        switch (_thisProperty.type)
         {
-            PerformInvasion();
+            case PropertyType.quarter:
+                if (!_thisProperty.dominated &&
+                    HasDominatedNeighbor() &&
+                    _thisProperty.GetSoldiers(SoldierType.Enemy) == 0 &&
+                    Random.Range(0f, 1f) < PropertyManager.Instance.invasionChancePerProperty &&
+                    _thisProperty.GetSoldiers(SoldierType.InProperty) > 0)
+                {
+                    PerformInvasion();
+                }
+                break;
+            default:
+                if (!_thisProperty.dominated &&
+                    HasDominatedNeighbor() &&
+                    _thisProperty.GetSoldiers(SoldierType.Enemy) == 0 &&
+                    Random.Range(0f, 1f) < PropertyManager.Instance.invasionChancePerProperty &&
+                    _thisProperty.GetSoldiers(SoldierType.InProperty) > 0 &&
+                    _thisProperty.GetSoldiers(SoldierType.InProperty) > (GetRandomDominatedNeighbor().GetSoldiers(SoldierType.InProperty) / 2))
+                {
+                    PerformInvasion();
+                }
+                break;
         }
     }
 
     private bool HasDominatedNeighbor()
     {
-        foreach (var neighbor in property.Neighbors)
+        foreach (var neighbor in _thisProperty.Neighbors)
         {
             if (neighbor.dominated && neighbor.GetSoldiers(SoldierType.ToGetOut) == 0)
             {
@@ -46,7 +66,7 @@ public class InvasionBehaviour : MonoBehaviour
     private Property GetRandomDominatedNeighbor()
     {
         List<Property> candidates = new List<Property>();
-        foreach (var neighbor in property.Neighbors)
+        foreach (var neighbor in _thisProperty.Neighbors)
         {
             if (neighbor.dominated && neighbor.GetSoldiers(SoldierType.ToGetOut) == 0)
             {
@@ -61,8 +81,8 @@ public class InvasionBehaviour : MonoBehaviour
     {
         // Attacks a random dominated neighbor with every soldier possible
         var target = GetRandomDominatedNeighbor();
-        int attackingSoldiers = property.GetSoldiers(SoldierType.InProperty);
-        target.AddSoldiers(SoldierType.Enemy, attackingSoldiers, new BattleInformation(property.kingdom, target.kingdom, attackingSoldiers, target.GetSoldiers(SoldierType.InProperty)));
-        property.AddSoldiers(SoldierType.ToGetOut, attackingSoldiers);
+        int attackingSoldiers = _thisProperty.GetSoldiers(SoldierType.InProperty);
+        target.AddSoldiers(SoldierType.Enemy, attackingSoldiers, new BattleInformation(_thisProperty.kingdom, target.kingdom, attackingSoldiers, target.GetSoldiers(SoldierType.InProperty)));
+        _thisProperty.AddSoldiers(SoldierType.ToGetOut, attackingSoldiers);
     }
 }
